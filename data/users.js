a4761e4687b2bd69fbb19e1ebd6e49372191e952
Upendra Kumar <toxic74412@gmail.com>
2023-08-15 11:51:23 -0700

    remove fakeuserlist from .env

---
+++ data/users.js
@@ -0,0 +1,44 @@
+const dummyUsers = [
+  {
+    id: 1001,
+    email: "tony@stark.io",
+    password: "test123",
+    username: "tony",
+    followers: [],
+    following: [],
+  },
+  {
+    id: 1002,
+    email: "captain@steve.roger",
+    password: "test123",
+    username: "captain",
+    followers: [],
+    following: [],
+  },
+  {
+    id: 1003,
+    email: "hulk@smart.banner",
+    password: "test123",
+    username: "hulk",
+    followers: [],
+    following: [],
+  },
+  {
+    id: 1004,
+    email: "hawkeye@avenger.io",
+    password: "test123",
+    username: "hawkeye",
+    followers: [],
+    following: [],
+  },
+  {
+    id: 1005,
+    email: "loki@mischief.com",
+    password: "test123",
+    username: "loki",
+    followers: [],
+    following: [],
+  },
+];
+
+export default dummyUsers;
